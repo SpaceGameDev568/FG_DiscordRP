d7258e78858a226a7e00e34d85af2047ba343fa4
Jesse Hodgson <space@spacegamedev.com>
2023-11-21 20:58:43 -0500

    =New biome thumbnails

---
+++ Source/FG_DiscordRP/Public/ReporterSubsystem.h
@@ -0,0 +1,51 @@
+// Copyright 2023 Jesse Hodgson. All Rights Reserved.
+
+// #pragma once
+//
+// #include "CoreMinimal.h"
+// #include "DiscordObject.h"
+// #include "FGPlayerController.h"
+// #include "Subsystem/ModSubsystem.h"
+// #include "ReporterSubsystem.generated.h"
+
+/**
+ * 
+ */
+// UCLASS()
+// class FG_DISCORDRP_API AReporterSubsystem : public AModSubsystem
+// {
+// 	GENERATED_BODY()
+//
+// public:
+//
+// 	AReporterSubsystem();
+//
+// protected:
+//
+// 	virtual void BeginPlay() override;
+//
+// public:
+//
+// 	virtual void Tick(float DeltaTime) override;
+//
+// 	UDiscordObject* DiscordObject;
+// 	float UpdateInterval;
+// 	bool SubsystemDisabled;
+// 	int NumPlayersInSession;
+// 	FString PresenceString;
+// 	FString TierString;
+// 	FString DetailsString;
+// 	FString StateString;
+// 	FString GameLanguage;
+// 	FString DiscordClientID;
+// 	static FUniqueNetIdRepl& PlayerNetID;
+// 	static APlayerController* playerController;
+//
+// 	UFUNCTION(BlueprintCallable, Category = "Discord")
+// 	void ProcessPresenceString();
+//
+// 	UFUNCTION(BlueprintCallable, Category = "Discord")
+// 	void UpdatePresenceState();
+//
+// };
+//
