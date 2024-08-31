bca59a20381c161717c1576929a861b5e3798cfb
Jesse Hodgson <space@spacegamedev.com>
2024-08-31 18:34:28 -0400

    Refactor code to follow Unreal Engine Coding Standards

--- Source/FG_DiscordRP/FG_DiscordRP.Build.cs
+++ Source/FG_DiscordRP/FG_DiscordRP.Build.cs
@@ -11,28 +11,21 @@ public class FG_DiscordRP : ModuleRules
 		PublicIncludePaths.AddRange(
 			new[]
 			{
-				// ... add public include paths required here ...
 				"DiscordSDK"
+				// ... add public include paths required here ...
 			}
 		);
-
-
-		PrivateIncludePaths.AddRange(
-			new string[]
-			{
-				// ... add other private include paths required here ...
-			}
-		);
-
 
 		PublicDependencyModuleNames.AddRange(
 			new[]
 			{
-				"Core", "DiscordUE4", "FactoryGame", "SML"
+				"Core",
+				"DiscordUE4",
+				"FactoryGame",
+				"SML"
 				// ... add other public dependencies that you statically link with here ...
 			}
 		);
-
 
 		PrivateDependencyModuleNames.AddRange(
 			new[]
@@ -42,14 +35,6 @@ public class FG_DiscordRP : ModuleRules
 				"Slate",
 				"SlateCore"
 				// ... add private dependencies that you statically link with here ...	
-			}
-		);
-
-
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
 			}
 		);
 	}
--- Source/FG_DiscordRP/Public/DRP_ConfigStruct.h
+++ Source/FG_DiscordRP/Public/DRP_ConfigStruct.h
@@ -6,34 +6,24 @@
 
 /* Struct generated from Mod Configuration Asset '/FG_DiscordRP/DRP_Config' */
 USTRUCT(BlueprintType)
-struct FDRP_ConfigStruct
-{
-	GENERATED_BODY()
-	UPROPERTY(BlueprintReadWrite)
-	float update_interval{};
+struct FDRP_ConfigStruct {
+    GENERATED_BODY()
+public:
+    UPROPERTY(BlueprintReadWrite)
+    float UpdateInterval{};
 
-	UPROPERTY(BlueprintReadWrite)
-	bool reset_discord_object{};
+    UPROPERTY(BlueprintReadWrite)
+    bool bAllowDebugLogging{};
 
-	UPROPERTY(BlueprintReadWrite)
-	bool disable_subsystem{};
-
-	UPROPERTY(BlueprintReadWrite)
-	bool debug_logging{};
-
-	UPROPERTY(BlueprintReadWrite)
-	bool is_developer{};
-
-	/* Retrieves active configuration value and returns object of this struct containing it */
-	static FDRP_ConfigStruct GetActiveConfig(UObject* WorldContext)
-	{
-		FDRP_ConfigStruct ConfigStruct{};
-		FConfigId ConfigId{"FG_DiscordRP", ""};
-		if (const UWorld* World = GEngine->GetWorldFromContextObject(WorldContext, EGetWorldErrorMode::ReturnNull))
-		{
-			UConfigManager* ConfigManager = World->GetGameInstance()->GetSubsystem<UConfigManager>();
-			ConfigManager->FillConfigurationStruct(ConfigId, FDynamicStructInfo{StaticStruct(), &ConfigStruct});
-		}
-		return ConfigStruct;
-	}
+    /* Retrieves active configuration value and returns object of this struct containing it */
+    static FDRP_ConfigStruct GetActiveConfig(UObject* WorldContext) {
+        FDRP_ConfigStruct ConfigStruct{};
+        FConfigId ConfigId{"FG_DiscordRP", ""};
+        if (const UWorld* World = GEngine->GetWorldFromContextObject(WorldContext, EGetWorldErrorMode::ReturnNull)) {
+            UConfigManager* ConfigManager = World->GetGameInstance()->GetSubsystem<UConfigManager>();
+            ConfigManager->FillConfigurationStruct(ConfigId, FDynamicStructInfo{FDRP_ConfigStruct::StaticStruct(), &ConfigStruct});
+        }
+        return ConfigStruct;
+    }
 };
+
